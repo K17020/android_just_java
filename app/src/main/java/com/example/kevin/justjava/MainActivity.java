01c5b995a105998257e7963902ef81018b6acaac
Kevin Tam <tam.kevin@hotmail.com>
2017-08-06 19:27:57 -0700

    Add basic functionality to +/- button to display a int when pressed

--- app/src/main/java/com/example/kevin/justjava/MainActivity.java
+++ app/src/main/java/com/example/kevin/justjava/MainActivity.java
@@ -9,11 +9,11 @@ package com.example.kevin.justjava;
  *
  */
 
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -30,7 +30,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        int quantity = 0;
+        display(quantity);
+        displayPrice(quantity);
     }
 
     /**
@@ -40,5 +42,22 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    public void increment (View view){
+        int quantity = 3;
+        display(quantity);
+    }
+    public void decrement(View view){
+        int quantity = 0;
+        display(quantity);
     }
 }
