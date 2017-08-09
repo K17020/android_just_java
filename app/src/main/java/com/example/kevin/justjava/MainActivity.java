065b4adde823cd83d8106053d332628f264e301b
Kevin Tam <tam.kevin@hotmail.com>
2017-08-09 15:20:36 -0700

    quantity buttons work!

--- app/src/main/java/com/example/kevin/justjava/MainActivity.java
+++ app/src/main/java/com/example/kevin/justjava/MainActivity.java
@@ -20,6 +20,8 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,11 +55,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void increment (View view){
-        int quantity = 3;
+        quantity = quantity + 1;
         display(quantity);
     }
     public void decrement(View view){
-        int quantity = 0;
+        quantity = quantity - 1;
         display(quantity);
     }
 }
